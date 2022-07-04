6926ba7747758dd137d0d41e0ef5621ad2b1f8a4
Cyprien Eckert <cipri.eckert@hotmail.fr>
2022-07-04 15:14:28 +0200

    add move create and board create and show controller logic

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,19 @@
 class BoardsController < ApplicationController
   def show
+    @board = Board.find(params[:id])
   end
 
   def create
-
+    other_user = User.where(username: params[:username])[0]
+    if other_user.valid?
+      board = Board.new
+      board.users << current_user
+      board.users << other_user
+      board.save
+      redirect_to board_path(board)
+    else
+      redirect_to root_path
+      flash[:alert] = 'Error'
+    end
   end
 end
--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,5 +1,25 @@
 class MovesController < ApplicationController
   def create
+    @board = Board.find(params[:board_id])
+    @move = Move.new(strong_params)
+    @move.board = @board
+    @move.user = current_user
+    @move.save
 
+    # BoardChannel.broadcast_to(
+    #   @board,
+    #   {
+    #     notation: params[:move][:notation],
+    #     fen: params[:move][:fen]
+    #   }
+    # )
+    # head :ok
+    # redirect_to board_path(@board)
+  end
+
+  private
+
+  def strong_params
+    params.require(:move).permit(:notation, :fen)
   end
 end
